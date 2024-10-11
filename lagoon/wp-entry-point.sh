10f2d13691bef60f10495903531a0bf0554578ba
Bryan Gruneberg <hello@bryangruneberg.com>
2024-10-11 10:57:20 -0600

    Bryan polishing WP

--- lagoon/wp-entry-point.sh
+++ lagoon/wp-entry-point.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+echo "Loading lagoon environment entrypoint"
+
+# Loading environment variables from .env and friends
+source /lagoon/entrypoints/50-dotenv.sh
+
+# Generate some additional enviornment variables
+source /lagoon/entrypoints/55-generate-env.sh
+
+echo "Done loading lagoon environment"
+
 if [ -z "$HTTP_HOST" ]; then
 	export HTTP_HOST=$LAGOON_ROUTE
 fi
