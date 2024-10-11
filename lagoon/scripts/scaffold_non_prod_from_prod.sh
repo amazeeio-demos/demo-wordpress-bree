9bef670ebe109fe70215ee35db4a0aff3ae327c3
Bryan Gruneberg <hello@bryangruneberg.com>
2024-10-11 11:13:13 -0600

    Bryan polishing WP

--- lagoon/scripts/scaffold_non_prod_from_prod.sh
+++ lagoon/scripts/scaffold_non_prod_from_prod.sh
@@ -8,7 +8,7 @@ WP_INSTALLED=$?
 
 if [ $WP_INSTALLED -ne 0 ]; then
   echo "Syncing the Main database for non-prod environment"
-  lagoon-sync sync mariadb -p demo-bree-wordpress -e main -t "$LAGOON_GIT_BRANCH" --verbose -y
+  lagoon-sync sync mariadb -p demo-bree-wordpress -e main -t "$LAGOON_GIT_BRANCH" --verbose --no-interaction
 else
   echo "WordPress is installed"
 fi
