f8e72c8b33b9286937508c5a89b21dcd89d4f9f1
Bree Benesh <bree.benesh@amazee.io>
2024-10-15 07:53:13 -0400

    commenting out files command on post rollout task for now (it fails).

--- lagoon/scripts/scaffold_non_prod_from_prod.sh
+++ lagoon/scripts/scaffold_non_prod_from_prod.sh
@@ -10,7 +10,7 @@ if [ $WP_INSTALLED -ne 0 ]; then
   echo "Syncing the Main database for non-prod environment"
   cd /app
   lagoon-sync sync mariadb -p $LAGOON_PROJECT -e main -t "$LAGOON_GIT_BRANCH" --verbose --no-interaction
-  lagoon-sync files mariadb -p $LAGOON_PROJECT -e main -t "$LAGOON_GIT_BRANCH" --verbose --no-interaction
+  # lagoon-sync files mariadb -p $LAGOON_PROJECT -e main -t "$LAGOON_GIT_BRANCH" --verbose --no-interaction
 else
   echo "WordPress is installed"
 fi
